8198752c76380d638e92ae772936723bccbf2426
rashidxon <nuriddinovrashidxon2002@gmail.com>
2023-11-28 15:52:19 +0500

    ZED

---
+++ main.js
@@ -0,0 +1,101 @@
+const btn = document.querySelector('.btn')
+const btn1 = document.querySelector('.btn1')
+const btn2 = document.querySelector('.btn2')
+const btn3 = document.querySelector('.btn3')
+const btn4 = document.querySelector('.btn4')
+const btn5 = document.querySelector('.btn5')
+const close_button = document.querySelector('.close_button')
+const close_button1 = document.querySelector('.close_button1')
+const close_button2 = document.querySelector('.close_button2')
+const close_button3 = document.querySelector('.close_button3')
+const close_button4 = document.querySelector('.close_button4')
+const close_button5 = document.querySelector('.close_button5')
+const modal_kompyuter = document.querySelector('.modal_kompyuter')
+const modal_smm = document.querySelector('.modal_smm')
+const modal_grafik = document.querySelector('.modal_grafik')
+const modal_android = document.querySelector('.modal_android')
+const modal_web = document.querySelector('.modal_web')
+const modal_py = document.querySelector('.modal_py')
+const overlay = document.querySelector('.overlay')
+
+const closed = document.querySelector('.closed')
+const closed1 = document.querySelector('.closed2')
+const closed3 = document.querySelector('.closed3')
+const closed4 = document.querySelector('.closed4')
+const closed5 = document.querySelector('.closed5')
+const closed6 = document.querySelector('.closed6')
+
+
+btn.addEventListener("click", ()=>{
+    modal_kompyuter.classList.toggle("hidden")
+    overlay.classList.toggle("hidden")
+})
+btn1.addEventListener("click", ()=>{
+    modal_smm.classList.toggle("hidden")
+    overlay.classList.toggle("hidden")
+})
+btn2.addEventListener("click", ()=>{
+    modal_grafik.classList.toggle("hidden")
+    overlay.classList.toggle("hidden")
+})
+btn3.addEventListener("click", ()=>{
+    modal_android.classList.toggle("hidden")
+    overlay.classList.toggle("hidden")
+})
+btn4.addEventListener("click", ()=>{
+    modal_web.classList.toggle("hidden")
+    overlay.classList.toggle("hidden")
+})
+btn5.addEventListener("click", ()=>{
+    modal_py.classList.toggle("hidden")
+    overlay.classList.toggle("hidden")
+})
+close_button.addEventListener("click", ()=>{
+    modal_kompyuter.classList.toggle("hidden")
+    overlay.classList.toggle("hidden")
+})
+close_button1.addEventListener("click", ()=>{
+    modal_smm.classList.toggle("hidden")
+    overlay.classList.toggle("hidden")
+})
+close_button2.addEventListener("click", ()=>{
+    modal_grafik.classList.toggle("hidden")
+    overlay.classList.toggle("hidden")
+})
+close_button3.addEventListener("click", ()=>{
+    modal_android.classList.toggle("hidden")
+    overlay.classList.toggle("hidden")
+})
+close_button4.addEventListener("click", ()=>{
+    modal_web.classList.toggle("hidden")
+    overlay.classList.toggle("hidden")
+})
+close_button5.addEventListener("click", ()=>{
+    modal_py.classList.toggle("hidden")
+    overlay.classList.toggle("hidden")
+})
+
+closed.addEventListener("click", ()=>{
+    modal_kompyuter.classList.toggle("hidden")
+    overlay.classList.toggle("hidden")
+})
+closed1.addEventListener("click", ()=>{
+    modal_smm.classList.toggle("hidden")
+    overlay.classList.toggle("hidden")
+})
+closed3.addEventListener("click", ()=>{
+    modal_grafik.classList.toggle("hidden")
+    overlay.classList.toggle("hidden")
+})
+closed4.addEventListener("click", ()=>{
+    modal_android.classList.toggle("hidden")
+    overlay.classList.toggle("hidden")
+})
+closed5.addEventListener("click", ()=>{
+    modal_web.classList.toggle("hidden")
+    overlay.classList.toggle("hidden")
+})
+closed6.addEventListener("click", ()=>{
+    modal_py.classList.toggle("hidden")
+    overlay.classList.toggle("hidden")
+})
